779b580a86cf02b167e7d11efa85b2dff9802aa7
nictengkk <nictengkk@gmail.com>
2019-03-22 17:21:35 +0800

    update index.js to include env variables for different stages, test, production and developement

--- models/index.js
+++ models/index.js
@@ -1,9 +1,24 @@
 const Sequelize = require("sequelize");
 //create the preconfigured sequelize instance
-const sequelize = new Sequelize("booksapi", "postgres", "", {
-  dialect: "postgres",
-  logging: false
-});
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
+let sequelize;
+//Connect to different database depending on env
+if (env === "production") {
+  sequelize = new Sequelize(config.url, config.options);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config.options
+  );
+}
+
+// const sequelize = new Sequelize("booksapi", "postgres", "", {
+//   dialect: "postgres",
+//   logging: false
+// });
 
 //pass the models to the connection
 const models = {
